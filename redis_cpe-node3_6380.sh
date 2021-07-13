30d65546c545d0f166e0ae50385ca776f1bf078b
Arun Prakash <sa.arunprakash@gmail.com>
2021-07-13 15:50:25 +0530

    Added line

--- redis_cpe-node3_6380.sh
+++ redis_cpe-node3_6380.sh
@@ -9,3 +9,4 @@ VALUE=$(/usr/local/bin/redis-cli -h 10.0.1.117 -p 6380 keys cpe:* | wc -l)
 	echo CPE count for PORT 6380 is $VALUE | mail -s "node3.cluster1.redis.owler.internal" arunprakash@owler-inc.com, thiyagarajan@owler-inc.com, tsheriff@owler-inc.com, oncall@owler.com;
 
 	fi
+## done
